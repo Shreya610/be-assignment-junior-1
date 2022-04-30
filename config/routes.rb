04e5d6433671d69b2b1f7a470b5d65a629e3ed89
Shreya610 <48761638+Shreya610@users.noreply.github.com>
2022-04-30 23:43:41 +0530

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static#dashboard"
   get 'people/:id', to: 'static#person'
+  post 'user_expense', to: 'user_expense#create'
+  post 'settle_up_expense', to: 'settle_up_expense#create'
+  resources :users
+  resources :user_expenses
+  resources :settle_up_expenses
+  resources :shared_expenses_between_users 
 end
